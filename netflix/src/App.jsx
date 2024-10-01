8c5a548c1137335565f564f8c9c71e69f94572bc
Kasim Nurlgn <kasimnurlgn9@gmail.com>
2024-10-01 10:28:20 +0300

    first-commit

---
+++ netflix/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>HI! this is Kasim, cloning Netflix</h1>
+    </>
+  );
+}
+
+export default App;
