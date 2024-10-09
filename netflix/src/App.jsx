0692fc24a56ca78980acbc0e3751cd748c04ca58
Kasim Nurlgn <kasimnurlgn9@gmail.com>
2024-10-09 10:19:16 +0300

    header and footer

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>HI! this is Kasim, cloning Netflix</h1>
+      <div className="App">
+        <Home />
+      </div>
     </>
   );
 }
---
+++ netflix/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+
+function Home() {
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
